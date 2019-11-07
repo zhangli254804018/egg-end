d2208b25de56fc30dbf8c79e7d7d489fb6db8f5f
zhangli <254804018@qq.com>
2019-11-08 01:21:31 +0800

    fix:add

--- config/config.default.js
+++ config/config.default.js
@@ -71,7 +71,7 @@ module.exports = appInfo => {
       // 用户名
       user: 'root',
       // 密码
-      password: 'chenyanqin1992',
+      password: '123456',
       // 数据库名
       database: 'test',
     },
