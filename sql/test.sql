079e452130146388620969e038d40de9fe9cc15a
chenlei07 <779550989@qq.com>
2020-02-01 19:50:44 +0800

    fix:调整数据sql语句

--- sql/test.sql
+++ sql/test.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 本地数据sql
-Source Server Version : 50727
+Source Server         : 本地数据库
+Source Server Version : 50729
 Source Host           : localhost:3306
 Source Database       : test
 
 Target Server Type    : MYSQL
-Target Server Version : 50727
+Target Server Version : 50729
 File Encoding         : 65001
 
-Date: 2020-01-08 11:39:35
+Date: 2020-02-01 19:48:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -27,7 +27,7 @@ CREATE TABLE `lottery` (
   `creat_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of lottery
@@ -38,6 +38,9 @@ INSERT INTO `lottery` VALUES ('大乐透', '23,23,3,23,4,4', null, '33', '2019-1
 INSERT INTO `lottery` VALUES ('大乐透', '1,2,3,4,5,6,67,7,8,8', null, '34', '2020-01-08 11:16:45', '2020-01-08 11:16:45');
 INSERT INTO `lottery` VALUES ('大乐透2020-1-8', '1,2,3,2,6,2,3,48', null, '35', '2020-01-08 11:17:22', '2020-01-08 11:17:22');
 INSERT INTO `lottery` VALUES ('大乐透2020-1-8 11:19:00', '1,2,3,2,6,2,3,48', null, '36', '2020-01-08 11:19:57', '2020-01-08 11:19:57');
+INSERT INTO `lottery` VALUES ('大乐透2010', '12,3,4,5,6,7', null, '37', '2020-02-01 17:21:09', '2020-02-01 17:21:09');
+INSERT INTO `lottery` VALUES ('string', 'string', null, '38', '2020-02-01 18:11:08', '2020-02-01 18:11:08');
+INSERT INTO `lottery` VALUES ('chenlei', 'abc', null, '39', '2020-02-01 19:21:31', '2020-02-01 19:21:31');
 
 -- ----------------------------
 -- Table structure for `user`
@@ -48,7 +51,7 @@ CREATE TABLE `user` (
   `password` varchar(60) NOT NULL,
   `uid` int(120) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of user
@@ -68,3 +71,5 @@ INSERT INTO `user` VALUES ('terst12', '2323323', '12');
 INSERT INTO `user` VALUES ('terst1254646', '2323323', '13');
 INSERT INTO `user` VALUES ('陈磊注册23', '2323323', '14');
 INSERT INTO `user` VALUES ('12', '12', '15');
+INSERT INTO `user` VALUES ('chenlei1992', 'chenlei1992', '16');
+INSERT INTO `user` VALUES ('123456789', '123456789', '17');
