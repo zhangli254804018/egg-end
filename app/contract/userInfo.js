8e1c5154b3fd3682f316c31a3931c67fd4947485
zhangli <254804018@qq.com>
2020-04-08 01:17:36 +0800

    fix:提交数组 

--- app/contract/userInfo.js
+++ app/contract/userInfo.js
@@ -6,7 +6,7 @@ module.exports = {
     username: { type: 'string', required: true },
     password: { type: 'string', required: true },
     gender: { type: 'string', enum: ['1', '2'] },
-    role: {  type: 'integer', enum: [0, 1, 2] }
+    role: {  type: 'array', itemType: 'string' }
   },
   loginUser: {
     username: { type: 'string', required: true },
