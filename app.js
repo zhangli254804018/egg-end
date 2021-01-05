737677f0360ca56572381927d5c8344153c20887
zhangli <254804018@qq.com>
2021-01-05 10:05:46 +0800

    feat:2021-01-05-10:05:43

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const egg = require('egg');
 
 const workers = Number(process.argv[2] || require('os').cpus().length);
 egg.startCluster({
-  workers,
-  baseDir: __dirname,
+    workers,
+    baseDir: __dirname
 });
